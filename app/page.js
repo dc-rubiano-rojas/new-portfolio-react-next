d2468ea1e514f4c3a1d40ae4f8d7bdaac1f01c5a
dc-rubiano-rojas <dcrubiano02@gmail.com>
2024-03-18 13:23:56 -0500

    :fire: remove adsense component call

--- app/page.js
+++ app/page.js
@@ -13,7 +13,6 @@ export default function Home() {
       <div className="container mx-auto px-12 py-4 mt-20">
         <HeroSection />
         <AchivmentSection />
-        <AdSense/>
         <AboutSection />
         <ProjectSection />
         <EmailSection />
